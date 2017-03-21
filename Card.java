46c997382d81f13c1657052ca69148de2a0e9517
Patrick Dorton <pdorton89@gmail.com>
2017-03-21 16:46:38 -0400

    Added a Cranium Class
    Made a few functions for the game,
    dice roller function
    function to select a random player
    gui to pull in data from the user on the number of players and all the
    error handling with it.
    blobzx is an import to use the sandbox to make a window of standard size
    that has a bunch of useful functions for little game gui's
    
    the images are the board which will be used for the background of the
    sandbox.
    
    the card example is for the artsy people to have as a layout

---
+++ Card.java
@@ -0,0 +1,10 @@
+
+public class Card 
+{
+	private int color;
+	private String text;
+	private String hint;
+	private String correctAnswer;
+	private String[] answers = new String[4];
+	
+}
---
+++ Cranium.java
@@ -0,0 +1,67 @@
+import java.util.*;
+import javax.swing.JOptionPane;
+import java.util.Random;
+import java.awt.event.*; // trim for only click events. 
+import blob.zx.SandBoxMode;
+import blob.zx.SandBox;
+
+
+
+
+
+
+
+
+// 15 spaces in game board
+
+
+public class Cranium 
+{
+	public static String[] rollList = {"Purple","Yellow","Red","Blue","Green","Purple"};
+	public static Scanner in = new Scanner(System.in);
+	private static int numPlayers;
+	private static int startingPlayer; 
+	private static int diceRolls;
+	private static JOptionPane game = new JOptionPane();
+	
+	
+	
+	public static void main(String[] args) 
+	{
+		setNumPlayers();
+		startingPlayer = getRandomNumber(numPlayers);
+	}
+	
+	public	 static void setNumPlayers()
+	{// sets the number of players for the game. 
+		int playerNum;
+		playerNum = Integer.parseInt(JOptionPane.showInputDialog("How many players will there be? \n\n (between 2 - 4)"));
+		if(playerNum < 2 || playerNum > 4 )
+		{// handles the user entering the incorrect number of players.
+			JOptionPane.showMessageDialog(null, "Invalid number of players selected", "alert", JOptionPane.ERROR_MESSAGE);
+			setNumPlayers();
+		}		
+		numPlayers = playerNum;
+	}
+	
+	public int getNumPlayers()
+	{// gets the number of players for the game. 
+		return numPlayers;
+	}
+	
+	private static int getRandomNumber(int numPlayers)
+	{// returns the random number to choose the player that starts the game. 
+		
+		Random random = new Random();
+		int randomNumber = random.nextInt()%numPlayers;
+		return randomNumber;
+	}
+	
+	private static int diceRoll()
+	{// a random number generator to simulate dice rolls
+		Random random = new Random();
+		int roll = random.nextInt()%6;
+		return roll -1; // to offset to find the position of the roll in the array. 
+	}
+	
+}
